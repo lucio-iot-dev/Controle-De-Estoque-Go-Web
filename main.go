cde22bdd3d6fbbaa7222357be670b422d315fdae
Lúcio do Nascimento <luciomate95@gmail.com>
2022-05-18 00:08:52 -0300

    Lendo informaçoes do bando de dados

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ func conectaComBancoDeDados() *sql.DB {
 }
 
 type Produto struct {
+	Id int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -28,20 +29,42 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
 
-	db := conectaComBancoDeDados()
-	defer db.Close()
-
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+   db := conectaComBancoDeDados()
 
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
-		{"Tenis", "Confortável", 89, 3},
-		{"Fone", "Muito bom", 59, 2},
-	}
+	 selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+    if err != nil {
+			panic(err.Error())
+		}
+
+		p := Produto{}
+		produtos := []Produto{}
+
+		for selectDeTodosOsProdutos.Next(){
+			var id, quantidade  int
+			var nome, descricao string
+			var preco float64
+
+			err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+			if err != nil {
+				panic(err.Error())
+			}
+
+			p.Nome = nome
+			p.Descricao = descricao
+			p.Preco = preco
+			p.Quantidade = quantidade
+
+			produtos = append(produtos, p)
+			
+		}
+
 
 	temp.ExecuteTemplate(w, "Index", produtos)
+	
+	defer db.Close()
 }
