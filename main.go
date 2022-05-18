a4ffc7112908b995f3b2015d30574632136c6ebc
Lúcio do Nascimento <luciomate95@gmail.com>
2022-05-17 23:11:07 -0300

    conexao com o banco de dados e melhoria do html

--- main.go
+++ main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
+
+func conectaComBancoDeDados() *sql.DB {
+	conexao := "user=luciodev dbname=loja password=123456 host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+
+	db := conectaComBancoDeDados()
+	defer db.Close()
+
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{"Tenis", "Confortável", 89, 3},
+		{"Fone", "Muito bom", 59, 2},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
