4897f1969b5c2d192c10bcc1e7c0ddd74cf1d624
Lúcio do Nascimento <luciomate95@gmail.com>
2022-05-23 20:53:50 -0300

    criaçao da funçao para inserirdados no banco de dados e atualizar no HTML os produtos cadastrados

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"Controle-De-Estoque-Go-Web/models"
+	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 )
 
@@ -18,4 +20,26 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 func New(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertidoParaFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço!")
+		}
+
+		quantidadeConvertidoParaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade!")
+		}
+
+		models.CriaNovoProduto(nome, descricao, precoConvertidoParaFloat, quantidadeConvertidoParaInt)
+	}
+	http.Redirect(w, r, "/", 301)
 }
--- models/produtos.go
+++ models/produtos.go
@@ -45,3 +45,16 @@ func BuscaTodosOsProdutos() []Produto {
 	return produtos
 
 }
+
+func CriaNovoProduto(nome, descricao string, preco float64, quantidade int) {
+	db := db.ConectaComBancoDeDados()
+
+	insereDadosNoBanco, err := db.Prepare("insert into produtos(nome, descricao, preco, quantidade) values($1, $2, $3, $4)")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	insereDadosNoBanco.Exec(nome, descricao, preco,quantidade)
+
+	defer db.Close()
+}
